28ca57279214dd29d38e2c73104735cf84cb1a77
Matthew Chau <chauchiuwai@yahoo.com>
2024-07-09 11:08:32 -0400

    Distributed Prover for EVM Circuit (#1)
    * Add support for EVM circuit in distributed prover
    ---------
    
    Co-authored-by: Matthew <chauchiuwai@yahoo.com>
    Co-authored-by: guyu <guyu@girogiro.xyz>

--- distributed-prover/run.sh
+++ distributed-prover/run.sh
@@ -2,12 +2,13 @@
 
 # Function to display usage
 usage() {
-  echo "Usage: $0 [--profile dev|release] <setup|prove|prove-local|verify> [prover_index]"
+  echo "Usage: $0 [--profile dev|release] <circuit> <setup|prove|prove-local|verify> [prover_index]"
   exit 1
 }
 
 # Initialize variables
 PROFILE="release"
+CIRCUIT=""
 COMMAND=""
 PROVER_INDEX=""
 
@@ -21,6 +22,13 @@ while [[ "$#" -gt 0 ]]; do
     --profile)
       PROFILE="$2"
       shift
+      ;;
+    evm|keccak)
+      if [[ -n "$CIRCUIT" ]]; then
+        usage
+      else
+        CIRCUIT="$1"
+      fi
       ;;
     setup|prove|prove-local|verify)
       if [[ -n "$COMMAND" ]]; then
@@ -41,6 +49,11 @@ if [[ -z "$COMMAND" ]]; then
   usage
 fi
 
+# Check if circuit is provided
+if [[ -z "$CIRCUIT" ]]; then
+  usage
+fi
+
 # Validate profile
 if [[ "$PROFILE" != "dev" && "$PROFILE" != "release" ]]; then
   echo "Invalid profile: $PROFILE. Must be 'dev' or 'release'."
@@ -53,6 +66,12 @@ if [[ "$COMMAND" != "setup" && "$COMMAND" != "prove" && "$COMMAND" != "prove-loc
   exit 1
 fi
 
+# Validate circuit
+if [[ "$CIRCUIT" != "evm" && "$CIRCUIT" != "keccak" ]]; then
+  echo "Invalid circuit: $CIRCUIT. Must be 'evm' or 'keccak'."
+  exit 1
+fi
+
 # If command is prove, check for prover index
 if [[ "$COMMAND" == "prove" && -z "$PROVER_INDEX" ]]; then
   echo "Prover index is required for the 'prove' command."
@@ -61,7 +80,7 @@ fi
 
 # Construct the cargo command
 if [[ "$COMMAND" == "prove" ]]; then
-  cargo run --package distributed-prover --profile "$PROFILE" "$COMMAND" "$PROVER_INDEX"
+  cargo run --package distributed-prover --profile "$PROFILE" "$CIRCUIT" "$COMMAND" "$PROVER_INDEX"
 else
-  cargo run --package distributed-prover --profile "$PROFILE" "$COMMAND"
+  cargo run --package distributed-prover --profile "$PROFILE" "$CIRCUIT" "$COMMAND"
 fi
--- distributed-prover/src/main.rs
+++ distributed-prover/src/main.rs
@@ -3,6 +3,11 @@ use std::env;
 mod circuits;
 mod util;
 
+enum Circuit {
+    EVM,
+    Keccak,
+}
+
 enum Command {
     Setup,
     Prove,
@@ -12,30 +17,56 @@ enum Command {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let command = match args.get(1).map(|s| s.as_str()) {
+
+    let circuit = match args.get(1).map(|s| s.as_str()) {
+        Some("evm") => Circuit::EVM,
+        Some("keccak") => Circuit::Keccak,
+        _ => {
+            eprintln!("Usage: {} <evm|keccak> <setup|prove|prove-local|verify>", args[0]);
+            std::process::exit(1);
+        }
+    };
+    let command = match args.get(2).map(|s| s.as_str()) {
         Some("setup") => Command::Setup,
         Some("prove") => Command::Prove,
         Some("prove-local") => Command::ProveLocal,
         Some("verify") => Command::Verify,
         _ => {
-            eprintln!("Usage: {} <setup|prove|verify>", args[0]);
+            eprintln!("Usage: {} <evm|keccak> <setup|prove|prove-local|verify>", args[0]);
             std::process::exit(1);
         }
     };
 
-    match command {
-        Command::Setup => {
-            circuits::keccak::setup();
-        }
-        Command::Prove => {
-            let prover_index = args.get(2).map(|s| s.parse().unwrap()).unwrap_or(0);
-            circuits::keccak::prove(prover_index);
-        }
-        Command::ProveLocal => {
-            circuits::keccak::prove_local();
-        }
-        Command::Verify => {
-            circuits::keccak::verify();
+    match circuit {
+        Circuit::EVM => match command {
+            Command::Setup => {
+                circuits::evm::setup();
+            }
+            Command::Prove => {
+                let prover_index = args.get(3).map(|s| s.parse().unwrap()).unwrap_or(0);
+                circuits::evm::prove(prover_index);
+            }
+            Command::ProveLocal => {
+                circuits::evm::prove_local();
+            }
+            Command::Verify => {
+                circuits::evm::verify();
+            }
+        },
+        Circuit::Keccak => match command {
+            Command::Setup => {
+                circuits::keccak::setup();
+            }
+            Command::Prove => {
+                let prover_index = args.get(3).map(|s| s.parse().unwrap()).unwrap_or(0);
+                circuits::keccak::prove(prover_index);
+            }
+            Command::ProveLocal => {
+                circuits::keccak::prove_local();
+            }
+            Command::Verify => {
+                circuits::keccak::verify();
+            }
         }
     }
 }
